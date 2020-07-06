7ca33007c8a5e32259d162588af110c446eb1fb9
Ian Gregg <iangregg188@gmail.com>
2020-07-06 14:52:32 -0700

    Updated DOM and BL  for the API's as well as styles elements.
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>
    Co-Authored-By: Ben Russell <benjaminrussell36@gmail.com>
    Co-authored-by: Joseph Pearce <joseph.pearce05@gmail.com>
    Co-authored-by: Sean Downs <spdowns@protonmail.com>

--- JS Business logic/plants.js
+++ JS Business logic/plants.js
@@ -1,7 +1,7 @@
 export class PlantService {
   async getPlant() {
     try {
-      let response = await fetch(`https://trefle.io/api/plants?token=${process.env.API_KEY}`);
+      let response = await fetch(`https://trefle.io/api/plants?token=${process.env.API_KEY}&drought_tolerance=medium`);
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.join();
@@ -9,6 +9,24 @@ export class PlantService {
         jsonifiedResponse = false;
       }
       return jsonifiedResponse;
+    } catch(error) {
+      return false;
+    }
+  }
+}
+
+
+export class ZipService {
+  async getZip() {
+    try {
+      let zipResponse = await fetch(`https://c0bra.api.stdlib.com/zipcode-to-hardiness-zone/?zipcode=${zipcode}`);
+      let jsonifiedZipResponse;
+      if (zipResponse.ok && zipResponse.status === 200) {
+        jsonifiedZipResponse = await zipResponse.join();
+      } else {
+        jsonifiedZipResponse = false;
+      }
+      return jsonifiedZipResponse;
     } catch(error) {
       return false;
     }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { ZipService } from './../JS Business logic/plants.js';
 
 $(document).ready(function() {
+  $("#").submit(function(event){
+    event.preventDefault();
+    
+  });
 });
