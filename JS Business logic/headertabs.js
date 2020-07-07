fbbb21272097e3a522ee77381810068a447f3243
Ian Gregg <iangregg188@gmail.com>
2020-07-07 11:23:34 -0700

    Working on making the tabs work but having issues with the content showing.
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>
    Co-authored-by: Ben Russell <benjaminrussell36@gmail.com>
    Co-authored-by: Joseph Pearce <joseph.pearce05@gmail.com>
    Co-authored-by: Sean Downs <spdowns@protonmail.com>
    Co-authored-by: Ian Gregg <iangregg188@gmail.com>

--- JS Business logic/headertabs.js
+++ JS Business logic/headertabs.js
@@ -1,5 +1,5 @@
 function openTabs(tabName, tabcontent) {
-  var i, tabcontent, .tablinks;
+  var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
