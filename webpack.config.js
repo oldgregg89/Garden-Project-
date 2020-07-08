d96835d45da5c81e3c4d9f23f9c99878d11a1936
Ben Russell <benjaminrussell36@gmail.com>
2020-07-08 15:53:30 -0700

    Get dropdowns to filter response and append cards to DOM.
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>
    co-authored-by: Sean Downs <spdowns@protonmail.com>
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>
    Co-authored-by: Ian Gregg <iangregg188@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -5,42 +5,42 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-entry: './src/main.js',
-output: {
-  filename: 'bundle.js',
-  path: path.resolve(__dirname, 'dist')
-},
-devtool: 'eval-source-map',
-devServer: {
-  contentBase: './dist'
-},
-plugins: [
-  new UglifyJsPlugin({ sourceMap: true }),
-  new CleanWebpackPlugin(),
-  new Dotenv(),
-  new HtmlWebpackPlugin({
-    title: 'Garden Planner',
-    template: './src/index.html',
-    inject: 'body'
-  })
-],
-module: {
-  rules: [
-    {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader'
-      ]
-    },
-    {
-      test: /\.js$/,
-      exclude: [
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),
+    new CleanWebpackPlugin(),
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      title: 'Garden Planner',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: [
           /node_modules/,
           /spec/
         ],
-      loader: "eslint-loader"
-    }
-  ]
-}
+        loader: "eslint-loader"
+      }
+    ]
+  }
 };
